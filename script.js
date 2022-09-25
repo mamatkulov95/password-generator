86df98620c9fdf588f05db53d13082315b6e4cc7
M_Coder <m.mamatkulov95@gmail.com>
2022-09-25 15:48:14 +0500

    basic js commands added

--- script.js
+++ script.js
@@ -92,4 +92,56 @@ const characters = [
   "/",
 ];
 
-let 
+let generateBtn = document.querySelector(".generate-btn");
+let password1 = document.querySelector(".gen-password1");
+let password2 = document.querySelector(".gen-password2");
+let randomLetter1 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter2 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter3 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter4 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter5 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter6 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter7 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter8 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter9 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter10 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter11 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter12 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter13 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter14 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter15 = characters[Math.floor(Math.random() * characters.length)];
+let randomLetter16 = characters[Math.floor(Math.random() * characters.length)];
+
+generateBtn.addEventListener("click", getRandomPassword);
+
+function symbolFilter(){
+    for(let i = 0; i < characters.length; i++){
+        if ()
+    }
+}
+
+function getRandomPassword() {
+  for (let i = 0; i < characters.length; i++) {
+    password1.textContent =
+      randomLetter1 +
+      randomLetter2 +
+      randomLetter3 +
+      randomLetter4 +
+      randomLetter5 +
+      randomLetter6 +
+      randomLetter7 +
+      randomLetter8;
+
+      password2.textContent =
+      randomLetter9 +
+      randomLetter10 +
+      randomLetter11 +
+      randomLetter12 +
+      randomLetter13 +
+      randomLetter14 +
+      randomLetter15 +
+      randomLetter16;
+      console.log("clicked")
+  }
+  }
+
